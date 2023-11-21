d83ff1d5488e9aaf6dcc1f49a14a44c5abb38bb1
jeinieldjs <silvestrejeiniel@gmail.com>
2023-11-21 21:00:57 +0800

    added article model. added validations for Post

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Post < ApplicationRecord
+class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true,
                     length: { minimum: 10, message: "must be at least 10 characters long" }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_123909) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_124301) do
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
