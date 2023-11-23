8cff412f5da626d3e28759bd6f534d7a2b0cbb35
jeinieldjs <silvestrejeiniel@gmail.com>
2023-11-24 01:04:39 +0800

    added name attribute to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :name, presence: true
   validates :body, presence: true, length: { minimum: 10, message: "must be at least 10 characters long" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_030407) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_165958) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
